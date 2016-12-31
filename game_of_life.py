bc09d71c9fc9554053ee246358d46b3414203143
Thun0 <pjankow6@gmail.com>
2016-12-31 18:43:28 +0100

    fixed classes, drawing squares

--- game_of_life.py
+++ game_of_life.py
@@ -5,29 +5,80 @@ import sys
 
 clock = pygame.time.Clock()
 MAX_FPS = 60
-MAP_SIZE = 30
-TILE_SIZE = 40
+MAP_WIDTH = 100
+MAP_HEIGHT = 80
+TILE_SIZE = 10
 
 class Map:
     """ map class """
     
     def __init__(self):
-        self.map = [[0 for i in range(MAP_SIZE)] for j in range(MAP_SIZE)]
+        self.map = [[0 for i in range(MAP_WIDTH)] for j in range(MAP_HEIGHT)]
         print("Initializing map")
 
     def draw(self, display):
-        for i in range(MAP_SIZE):
-            for j in range(MAP_SIZE):
-                pygame.draw.rect(display.background, (0, i*8, 0),
-                    (i*TILE_SIZE, j*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+        for i in range(MAP_HEIGHT):
+            for j in range(MAP_WIDTH):
+                color = 0
+                if self.map[i][j] == 0:
+                    color = 255
+                pygame.draw.rect(display.background, (0, 0, 0),
+                                 (j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                pygame.draw.rect(display.background, (color, color, color),
+                                 (j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE-1, TILE_SIZE-1))
 
+    def countNeighbours(self, old, tileX, tileY):
+        neighbours = 0
+        if tileX > 0:
+            neighbours += old[tileX-1][tileY]
+            if tileY > 0:
+                neighbours += old[tileX-1][tileY-1]
+            if tileY < MAP_WIDTH-1:
+                neighbours += old[tileX-1][tileY+1]
+        if tileX < MAP_HEIGHT-1:
+            neighbours += old[tileX+1][tileY]
+            if tileY > 0:
+                neighbours += old[tileX+1][tileY-1]
+            if tileY < MAP_WIDTH-1:
+                neighbours += old[tileX+1][tileY+1]
+        if tileY > 0:
+            neighbours += old[tileX][tileY-1]
+        if tileY < MAP_WIDTH-1:
+                neighbours += old[tileX][tileY+1]
+        return neighbours
 
+    def update(self):
+        old = [x[:] for x in self.map]
+        for i in range(MAP_HEIGHT):
+            for j in range(MAP_WIDTH):
+                neighbours = self.countNeighbours(old, i, j)
+                if self.map[i][j] == 1:
+                    if neighbours == 2 or neighbours == 3:
+                        self.map[i][j] = 1
+                    else:
+                        self.map[i][j] = 0
+                elif neighbours == 3:
+                    self.map[i][j] = 1
+                else:
+                    self.map[i][j] = 0
+
+    def paint(self, tileX, tileY):
+        self.map[tileY][tileX] = Settings.painting
+
+    def setPainting(self):
+        (mouseX, mouseY) = pygame.mouse.get_pos()
+        tileX = int(mouseX / TILE_SIZE)
+        tileY = int(mouseY / TILE_SIZE)
+        if self.map[tileY][tileX] == 0:
+            Settings.painting = 1
+        else:
+            Settings.painting = 0
 
 class Display:
 
     def __init__(self):
         print("Initializing display")
-        self.resolution = (800, 600)
+        self.resolution = (MAP_WIDTH*TILE_SIZE, MAP_HEIGHT*TILE_SIZE)
         self.screen = pygame.display.set_mode(self.resolution)
         self.background = pygame.Surface(self.screen.get_size())
         self.background.fill((255, 255, 255))
@@ -36,22 +87,30 @@ class Display:
     def blit(self):
         self.screen.blit(self.background, (0, 0))
 
+class Settings:
+    mousePressed = 0
+    painting = 1
+    started = False
+    delay = 50
 
-def getKeys(event, isdown):
-    if isdown:
-        print("Key pressed")
-    else:
-        print("Key released")
+def handleInput(map):
 
-
-def handleInput():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            getKeys(event, True)
-        elif event.type == pygame.KEYUP:
-            getKeys(event, False)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            Settings.started = not Settings.started
+            if Settings.started:
+                print("Simulation started")
+            else:
+                print("Simulation stopped")
+        if not Settings.started:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Settings.mousePressed = True
+                map.setPainting();
+            elif event.type == pygame.MOUSEBUTTONUP:
+                Settings.mousePressed = False
 
 
 def init():
@@ -62,11 +121,18 @@ def init():
     return map, display;
 
 
-def update(delta):
+def update(delta, map):
     global clock
     text = "FPS: {0:.2f}".format(clock.get_fps())
     pygame.display.set_caption(text)
-
+    (mouseX, mouseY) = pygame.mouse.get_pos()
+    tileX = int(mouseX / TILE_SIZE)
+    tileY = int(mouseY / TILE_SIZE)
+    if Settings.mousePressed:
+        map.paint(tileX, tileY)
+    if Settings.started:
+        map.update()
+        pygame.time.wait(50)
 
 def redraw(map, display):
     map.draw(display)
@@ -78,12 +144,10 @@ def loop(map, display):
     global clock
     global MAX_FPS
     
-    print("Starting simulation")
-    
     while True:
         delta = clock.tick(MAX_FPS)
-        handleInput()
-        update(delta)
+        handleInput(map)
+        update(delta, map)
         redraw(map, display)
 
 
