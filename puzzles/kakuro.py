630e87cf0fd1590d9520d5245ada9f4b28f5f2ba
Thun <pjankow6@gmail.com>
2017-04-16 17:47:46 +0200

    Kakuro - inserting values

--- puzzles/kakuro.py
+++ puzzles/kakuro.py
@@ -9,7 +9,7 @@ FRAME_WIDTH = 1
 WIDTH = 600
 HEIGHT = 600
 FONT = "ubuntu"
-drawPossibilities = True
+drawPossibilities = False
 
 
 class Tile:
@@ -37,6 +37,7 @@ class Tile:
         elif val == 4:
             self.type = self.SUM_BOTH
 
+
     def getPossibilitiesCount(self):
         sum = 0
         for possible in self.possible:
@@ -54,6 +55,7 @@ class Tile:
     def isPossible(self, val):
         return self.possible[val]
 
+
 class Map:
     map = []
     downTiles = []
@@ -61,6 +63,7 @@ class Map:
     width = 0
     height = 0
     active = (-1, -1)
+    sums = []
 
     def __init__(self, display, filename):
         self.display = display
@@ -88,9 +91,113 @@ class Map:
                 self.rightTiles[idx].sumRight = value;
                 idx += 1
 
+        self.initSums()
         self.sumFont = pygame.font.SysFont(FONT, int(TILE_SIZE / 2))
         self.possibilitiesFont = pygame.font.SysFont(FONT, int(TILE_SIZE / 3.5))
         self.valueFont = pygame.font.SysFont(FONT, int(TILE_SIZE / 1.5))
+
+    def initSums(self):
+        sum2 = [[] in range(3)]
+        sum2.append([1, 2])
+        sum2.append([1, 3])
+        sum2.append([1, 2, 3, 4])
+        sum2.append([1, 2, 4, 5])
+        sum2.append([1, 2, 3, 4, 5, 6])
+        sum2.append([1, 2, 3, 5, 6, 7])
+        sum2.append([1, 2, 3, 4, 5, 6, 7, 8])
+        sum2.append([1, 2, 3, 4, 6, 7, 8, 9])
+        sum2.append([2, 3, 4, 5, 6, 7, 8, 9])
+        sum2.append([3, 4, 5, 7, 8, 9])
+        sum2.append([4, 5, 6, 7, 8, 9])
+        sum2.append([5, 6, 8, 9])
+        sum2.append([6, 7, 8, 9])
+        sum2.append([7, 9])
+        sum2.append([8, 9])
+
+        sum3 = [[] in range(6)]
+        sum3.append([1, 2, 3])
+        sum3.append([1, 2, 4])
+        sum3.append([1, 2, 3, 4, 5])
+        sum3.append([1, 2, 3, 4, 5, 6])
+        sum3.append([1, 2, 3, 4, 5, 6, 7])
+        sum3.append([1, 2, 3, 4, 5, 6, 7, 8])
+        for i in range(7):
+            sum3.append([i in range(1, 10)])
+        sum3.append([2, 3, 4, 5, 6, 7, 8, 9])
+        sum3.append([3, 4, 5, 6, 7, 8, 9])
+        sum3.append([4, 5, 6, 7, 8, 9])
+        sum3.append([5, 6, 7, 8, 9])
+        sum3.append([6, 8, 9])
+        sum3.append([7, 8, 9])
+
+        sum4 = [[]in range(10)]
+        sum4.append([1, 2, 3, 4])
+        sum4.append([1, 2, 3, 5])
+        sum4.append([1, 2, 3, 4, 5, 6])
+        sum4.append([1, 2, 3, 4, 5, 6, 7])
+        sum4.append([1, 2, 3, 4, 5, 6, 7, 8])
+        for i in range(11):
+            sum4.append([j for j in range(1, 10)])
+        sum4.append([2, 3, 4, 5, 6, 7, 8, 9])
+        sum4.append([3, 4, 5, 6, 7, 8, 9])
+        sum4.append([4, 5, 6, 7, 8, 9])
+        sum4.append([5, 7, 8, 9])
+        sum4.append([6, 7, 8, 9])
+
+        sum5 = [[]in range(15)]
+        sum5.append([i for i in range(1, 6)])
+        sum5.append([1, 2, 3, 4, 6])
+        sum5.append([i in range(1, 8)])
+        sum5.append([i in range(1, 9)])
+        for i in range(13):
+            sum5.append([j for j in range(1, 10)])
+        sum5.append([i in range(2, 10)])
+        sum5.append([i in range(3, 10)])
+        sum5.append([4, 6, 7, 8, 9])
+        sum5.append([5, 6, 7, 8, 9])
+
+        sum6 = [[]in range(21)]
+        sum6.append([1, 2, 3, 4, 5, 6])
+        sum6.append([1, 2, 3, 4, 5, 7])
+        sum6.append([i for i in range(1, 9)])
+        for i in range(13):
+            sum6.append([j for j in range(1, 10)])
+        sum6.append([i in range(2, 10)])
+        sum6.append([3, 5, 6, 7, 8, 9])
+        sum6.append([4, 5, 6, 7, 8, 9])
+
+        sum7 = [[] in range(28)]
+        sum7.append([1, 2, 3, 4, 5, 6, 7])
+        sum7.append([1, 2, 3, 4, 5, 6, 8])
+        for i in range(11):
+            sum7.append([j for j in range(1, 10)])
+        sum7.append([2, 4, 5, 6, 7, 8, 9])
+        sum7.append([3, 4, 5, 6, 7, 8, 9])
+
+        sum8 = [[] in range(36)]
+        sum8.append([1, 2, 3, 4, 5, 6, 7, 8])
+        sum8.append([1, 2, 3, 4, 5, 6, 7, 9])
+        sum8.append([1, 2, 3, 4, 5, 6, 8, 9])
+        sum8.append([1, 2, 3, 4, 5, 7, 8, 9])
+        sum8.append([1, 2, 3, 4, 6, 7, 8, 9])
+        sum8.append([1, 2, 3, 5, 6, 7, 8, 9])
+        sum8.append([1, 2, 4, 5, 6, 7, 8, 9])
+        sum8.append([1, 3, 4, 5, 6, 7, 8, 9])
+        sum8.append([2, 3, 4, 5, 6, 7, 8, 9])
+
+        sum9 = [[] in range(45)]
+        sum9.append([i in range(1, 10)])
+
+        self.sums.append([])
+        self.sums.append([])
+        self.sums.append([sum2])
+        self.sums.append([sum3])
+        self.sums.append([sum4])
+        self.sums.append([sum5])
+        self.sums.append([sum6])
+        self.sums.append([sum7])
+        self.sums.append([sum8])
+        self.sums.append([sum9])
 
     def drawPossibility(self, i, j, val):
         color = (0, 0, 0)
@@ -133,6 +240,8 @@ class Map:
         return tileX, tileY
 
     def draw(self):
+        global drawPossibilities
+
         for i in range(self.width):
             for j in range(self.height):
                 color = (255, 255, 255)
@@ -157,26 +266,41 @@ class Map:
                     if self.map[i][j].value != -1:
                         self.drawValue(i, j)
                     else:
-                        for k in range(10):
-                            if self.map[i][j].isPossible(k):
-                                self.drawPossibility(i, j, k)
+                        if drawPossibilities:
+                            for k in range(10):
+                                if self.map[i][j].isPossible(k):
+                                    self.drawPossibility(i, j, k)
                 else:
                     self.drawSum(i, j)
 
+    def drawValue(self, i, j):
+        color = (0, 0, 0)
+        textVal = self.valueFont.render(str(self.map[i][j].value), True, color)
+        textSize = self.valueFont.size(str(self.map[i][j].value))
+        textX = j * TILE_SIZE + TILE_SIZE /2 - textSize[0] / 1.75
+        textY = i * TILE_SIZE + TILE_SIZE / 2 - textSize[1] / 1.8
+        self.display.background.blit(textVal, (textX, textY))
+
     def deactivate(self):
-        self.map[self.active[1]][self.active[0]].active = False
+        self.map[self.active[0]][self.active[1]].active = False
         self.active = (-1, -1)
 
     def activate(self, i, j):
-        if self.map[j][i].type != Tile.EMPTY or self.active == (i, j):
-            print("JEST")
+        if self.map[j][i].type != Tile.EMPTY or self.active == (j, i):
             self.deactivate()
             return
         if self.active != (-1, -1):
-            self.map[self.active[1]][self.active[0]].active = False
-        self.active = (i, j)
+            self.map[self.active[0]][self.active[1]].active = False
+        self.active = (j, i)
         self.map[j][i].active = True
 
+    def insertValue(self, val):
+        if self.active != (-1, -1):
+            self.map[self.active[0]][self.active[1]].value = val
+
+    def deleteValue(self):
+        if self.active != (-1, -1):
+            self.map[self.active[0]][self.active[1]].value = -1
 
 
 class Display:
@@ -193,16 +317,28 @@ class Display:
 
 
 def handleInput(map):
+    global drawPossibilities
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            tileX, tileY = map.getIndices(x, y)
-            if tileX != -1:
-                map.activate(tileX, tileY)
-
+            if event.button == 1:
+                x, y = pygame.mouse.get_pos()
+                tileX, tileY = map.getIndices(x, y)
+                if tileX != -1:
+                    map.activate(tileX, tileY)
+            if event.button == 3:
+                drawPossibilities = not drawPossibilities
+        if event.type == pygame.KEYDOWN:
+            val = event.key - 48
+            if 1 <= val <= 9:
+                map.insertValue(val)
+            if event.key == pygame.K_DELETE or event.key == pygame.K_BACKSPACE:
+                map.deleteValue()
+            if event.key == pygame.K_ESCAPE:
+                map.deactivate()
 
 
 def init():
